94674e635d47b92ea28d2c4c9fd257af1a9f256d
Hom Dahal <homdahal997@gmail.com>
2024-06-05 12:51:14 -0400

    css updated as required

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Search from "./search";
 
 function HomePage(){
     return(
         <div className="HomePage">
             <Header title =  "Employee Directory" />
+            <Search />
         </div>
     )
 }
