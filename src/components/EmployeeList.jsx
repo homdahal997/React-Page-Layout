d2167252f2e95c9c7f7fce4a71386db2cba0f7cf
Hom Dahal <homdahal997@gmail.com>
2024-06-07 22:10:52 -0400

    Updated CSS classes and styles for EmployeeList and EmployeeListItem components

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -8,6 +8,31 @@ function EmployeeList(){
         name = "James King"
         title = "President and CEO"
         />
+        <EmployeeListItem
+        src = "https://ashallendesign.co.uk/images/custom/ash-on-a-laptop.png"
+        name = "Julie Taylor"
+        title = "VP of Marketing"
+        />
+        <EmployeeListItem
+        src = "https://ashallendesign.co.uk/images/custom/ash-on-a-laptop.png"
+        name = "Eugene Lee"
+        title = "CFO"
+        />
+        <EmployeeListItem
+        src = "https://ashallendesign.co.uk/images/custom/ash-on-a-laptop.png"
+        name = "John Williams"
+        title = "VP of Engineering"
+        />
+        <EmployeeListItem
+        src = "https://ashallendesign.co.uk/images/custom/ash-on-a-laptop.png"
+        name = "Ray Moore"
+        title = "VP of Sales"
+        />
+        <EmployeeListItem
+        src = "https://ashallendesign.co.uk/images/custom/ash-on-a-laptop.png"
+        name = "Paul Jones"
+        title = "QA Manager"
+        />
         </>
     )
 }
--- src/components/EmployeeListItem.jsx
+++ src/components/EmployeeListItem.jsx
@@ -4,7 +4,7 @@ function EmployeeListItem(props){
         <img className="profilePhoto" src = {props.src} alt={props.name} />
         <div className="profileName">
             <h3>{props.name}</h3>
-            <h4>{props.title}</h4>
+            <h6>{props.title}</h6>
         </div>
     </div>
     )
